09d631879e137eb0ac902e9d48ac32622887f280
JacobLei <17763636521@163.com>
2019-05-28 14:50:48 +0800

    修改第15题三树之和

--- src/main/java/A15_3Sum.java
+++ src/main/java/A15_3Sum.java
@@ -5,45 +5,63 @@ import java.util.Arrays;
 import java.util.List;
 
 public class A15_3Sum {
+
+    // 存储满足条件的三元组
+    List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> threeSum(int[] nums){
-        List<List<Integer>> res = new ArrayList<>();
+
         if(nums == null || nums.length < 3)
             return res;
-
+        // 给数组排序
         Arrays.sort(nums);
 
-        for(int i=0; (i<nums.length-2) && nums[i] <= 0; ++i){      // nums.length-2的目的是最后两个数不需要计算
-            if(i == 0 || nums[i] > nums[i-1]){    // 去掉重复固定的数，这里固定的数位nums[i]
-                int start = i + 1;
-                int end = nums.length - 1;
+        int len = nums.length;
 
-                while(start < end){
-                    int stand = nums[i] + nums[start] + nums[end];
-                    if(stand == 0){
-                        ArrayList<Integer> tmp = new ArrayList<Integer>();
-                        tmp.add(nums[i]);
-                        tmp.add(nums[start]);
-                        tmp.add(nums[end]);
-                        res.add(tmp);
-                        start++;
-                        end--;
-                        // 去掉重复数
-                        while(start<end && nums[start] == nums[start-1]){
-                            start++;
-                        }
-                        while(start<end && nums[end] == nums[end-1]){
-                            end--;
-                        }
-                    }else if(stand>0){
-                        end--;
-                    }else{
-                        start++;
-                    }
-                }
-            }
+        // 注意遍历的右边界条件
+        for (int i=0; i<len-2; i++){
+            // 去掉重复值
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+
+            // 由于数组已排序，当nums[i]>0,则它后面的数都大于零，不需要继续查找
+            if (nums[i] > 0)
+                break;
+            // 在数组中查找两个数与nums[i]的和为0
+            find(nums, i+1, len-1, nums[i]);
         }
 
         return res;
+    }
+
+    // 查找函数
+    private void find(int[] nums, int begin, int end, int target){
+        int left = begin, right = end;
+        while (left < right){
+            // 查找满足条件的三个
+            if(nums[left] + nums[right] + target == 0){
+                // 保存这个三个数
+                List<Integer> tmp = new ArrayList<>();
+                tmp.add(target);
+                tmp.add(nums[left]);
+                tmp.add(nums[right]);
+                res.add(tmp);
+
+                // 去掉重复的值
+                while (left < right && nums[left] == nums[left+1])
+                    left++;
+                while (left < right && nums[right] == nums[right-1])
+                    right--;
+
+                // 移动左右指针
+                left++;
+                right--;
+            }else if(nums[left] + nums[right] + target < 0){    // 当这个三个之和小于0，则左指针向右移动
+                left++;
+            }else { // 当这三个数大于零，则右指针向左移动
+                right--;
+            }
+        }
     }
 
     public static void main(String[] args) {
