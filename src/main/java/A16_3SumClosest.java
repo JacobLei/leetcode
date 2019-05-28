f9ad31031e44414ea44013cc364c83bfe31c7c38
JacobLei <17763636521@163.com>
2019-05-28 22:21:00 +0800

    修改第16题最接近的三数之和

--- src/main/java/A16_3SumClosest.java
+++ src/main/java/A16_3SumClosest.java
@@ -1,29 +1,43 @@
 import java.util.Arrays;
 
 public class A16_3SumClosest {
-    public int threeSumClosest(int[] nums, int target){
-        int colse = Integer.MAX_VALUE;
-        int diff = Integer.MAX_VALUE;
 
+    public int threeSumClosest(int[] nums, int target) {
+
+        int sum =0;    // 表示数组中三个元素的和
+        int dis = Integer.MAX_VALUE;   // 表示数组中三个元素的和与target的距离，即|sum-target|
+
+        // 对数组进行排序
         Arrays.sort(nums);
+        // 遍历数组
         for(int i=0; i<nums.length-2; ++i){
-            int left = i+1;
-            int right = nums.length - 1;
-            while(left < right){
-                int tmp = nums[i] + nums[left] + nums[right] - target;
-                if(Math.abs(tmp) < diff){
-                    diff = Math.abs(tmp);
-                    colse = tmp+target;
+            // 定义左指针和右指针
+            int left = i+1, right = nums.length-1;
+
+            while(left<right){
+                // 数组中三个元素的和与target的差，即sum-target
+                int diff = nums[i] + nums[left] + nums[right] - target;
+
+                // 当sum-target == 0，则说明已经找到与target最近的数了（即target本身）
+                if (diff == 0)
+                    return target;
+
+                // 寻找最小距离
+                if(Math.abs(diff) < dis){
+                    dis = Math.abs(diff);
+                    sum = diff + target;
                 }
-                if(tmp>0){
-                    right--;
+
+                // 移动左右指针
+                if(diff > 0){
+                    --right;
                 }else{
-                    left++;
+                    ++left;
                 }
             }
         }
 
-        return colse;
+        return sum;
     }
 
     public static void main(String[] args) {
